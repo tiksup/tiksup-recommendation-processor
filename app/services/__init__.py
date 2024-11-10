1625ff0ed9e692dd918e3acfd7732a9d4dec1248
jsusmachaca <falcorgd@gmail.com>
2024-11-10 11:58:46 -0500

    perf: deletes duplicate files

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,3 @@
-from .interactions import InteractionsService
+from .grpc.interactions import InteractionsService
+from .recommendations.affinity import Affinity
+from .recommendations.recommender import Recommender
